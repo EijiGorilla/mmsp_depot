048a933d00c236d6290c8699aa328a92cfbdadc3
EijiGorilla <emasuz@hotmail.com>
2024-10-16 08:04:15 +0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,10 @@ import '@esri/calcite-components/dist/components/calcite-shell';
 import '@esri/calcite-components/dist/components/calcite-list';
 import '@esri/calcite-components/dist/components/calcite-list-item';
 import '@esri/calcite-components/dist/components/calcite-shell-panel';
+import '@esri/calcite-components/dist/components/calcite-switch';
 import '@esri/calcite-components/dist/components/calcite-action';
+import '@esri/calcite-components/dist/components/calcite-tabs';
+import '@esri/calcite-components/dist/components/calcite-panel';
 import '@esri/calcite-components/dist/components/calcite-action-bar';
 import '@esri/calcite-components/dist/calcite/calcite.css';
 import {
@@ -230,14 +233,7 @@ function App() {
             style={{ width: '18vw' }}
             hidden
           >
-            <CalciteList>
-              <CalciteListItem
-                label=""
-                description=""
-                value="land-acquisition"
-                ref={layerListDiv}
-              ></CalciteListItem>
-            </CalciteList>
+            <div id="layers-container" ref={layerListDiv}></div>
           </CalcitePanel>
 
           <CalcitePanel
@@ -247,14 +243,7 @@ function App() {
             style={{ width: '18vw' }}
             hidden
           >
-            <CalciteList>
-              <CalciteListItem
-                label=""
-                description=""
-                value="basemaps"
-                ref={calcitePanelBasemaps}
-              ></CalciteListItem>
-            </CalciteList>
+            <div id="basemap-container" ref={calcitePanelBasemaps}></div>
           </CalcitePanel>
 
           <CalcitePanel heading="Description" data-panel-id="information" hidden>
